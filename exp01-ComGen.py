05203be5f48ad368c55fd79a1a98421d82c88956
David Soh <sotong0221@gmail.com>
2018-04-20 19:29:01 +0800

    add exp01

---
+++ exp01-ComGen.py
@@ -0,0 +1,10 @@
+# Combination generator
+# Configuration file of this exp is exp01-ComGen/config.json
+import pprint
+
+# initialize variable
+inpuseta = [] # input set
+
+print("NOW: Reading input file from data/input.txt")
+
+inpuseta = list(open('data/input.txt').read().splitlines())
