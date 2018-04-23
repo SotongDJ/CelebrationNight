9a2acd48cf1ba198e6e37d26a3e6b391a6fc4970
David Soh <sotong0221@gmail.com>
2018-04-23 14:28:24 +0800

    add arguli description and change -se to -li

--- exp02-q2a-batch.py
+++ exp02-q2a-batch.py
@@ -6,7 +6,7 @@ Title:
 Original command:
     time <bin>/fastq_to_fasta -v -Q33 -i <fastq file> -o <fasta file>
 Note:
-    command split into [linose,lino,linase,lina,lisi]
+    command split into [linoli,lino,linali,lina,lisi]
 Required files:
     ./list.txt
         # Path of File list
@@ -15,18 +15,23 @@ Required files:
 
 --- README ---
 """
-linose = ['time','./fastq_to_fasta','-v','-Q33','-i',]
-linase = ['-o']
+linoli = ['time','./fastq_to_fasta','-v','-Q33','-i',]
+linali = ['-o']
 
 # lino = fastq filename
 for lino in open("list.txt","r").read().splitlines():
     if "#" not in lino:
+
         # lina = fasta filename (and replace '_' with '-')
         lina = lino.replace('_','-').replace('fastq','fasta')
+
         # lisi = log file (and replace '_' with '-')
         lisi =  lino.replace('_','-').replace('fastq','log')
-        argumase = \
-        linose + [lino] + \
-        linase + [lina]
-        print(argumase)
-        call(argumase, stdout=open(lisi, 'w'))
+
+        # arguli = final command line in list type
+        arguli = \
+        linoli + [lino] + \
+        linali + [lina]
+
+        print(arguli)
+        call(arguli, stdout=open(lisi, 'w'))
