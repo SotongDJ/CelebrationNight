21e2722a1abd9d598397b1fd7e222e1f3b954e00
David Soh <sotong0221@gmail.com>
2018-04-26 00:17:04 +0800

    still consider solution...

--- exp06-fastqc-summary-polymerize.py
+++ exp06-fastqc-summary-polymerize.py
@@ -20,26 +20,17 @@ resutdi = {}
 resutsi = pafwasi + '/result.html'
 resutfa = open(resutsi,'w')
 
-for grupo in grupoli:
-    primadi = {}
-    secondi = {}
-    for fale in list(faledi.keys()):
-        if grupo in fale:
-
-            primadi = resutdi.get(grupo,{})
-
-            for tribe in tribeli:
-                prefisi = ""
-                prefisi = prefisdi.get(tribe,"")
-
-                if prefisi != "":
-                    if prefisi in fale:
-                        metali = []
-                        metali = secondi.get(tribe,[])
-                        metali.append(fale)
-                        secondi.update({ tribe : metali })
-                else:
-                    metali = []
-                    metali = secondi.get("",[])
-                    metali.append(fale)
-                    secondi.update({ "" : metali })
+resutdi={
+    group:{
+        tribe:[
+            fale,
+            fale
+        ]
+    },
+    group:{
+        tribe:[
+            fale,
+            fale
+        ]
+    }
+}
