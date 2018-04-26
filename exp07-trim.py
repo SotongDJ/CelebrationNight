579d39167147e85a928231988bd97703b7baee62
David Soh <sotong0221@gmail.com>
2018-04-26 17:58:46 +0800

    exp07: stuck at some point, rethink the workflow and prepare to redesign command

--- exp07-trim.py
+++ exp07-trim.py
@@ -1,14 +1,14 @@
-import sys
+import sys, json, pprint
 from subprocess import call
 
-helber=""""
+helber="""
    --- README of exp07-trim ---
 Title:
     batch processing for Trimmomatic
 Usage:
     python exp07-trim <source>
 Note:
-    command split into [ linoli , lina , linali ]
+    command split into [ linoli , linuli , linali ]
 Required variables:
   config.json
     "var"
@@ -48,21 +48,44 @@ configsi = 'data/config.json'
 configfa = open(configsi,'r')
 configso = json.load(configfa)
 configdi = configso.get('var',{})
+"""
+patoho: path of fastq directory
+binoho: <bin>
+resuho: path of output directory
+tredasi: threads """
+pafwadi = configdi.get('path',{})
+patoho = pafwadi.get('pwd',"")
+soroho = patoho + "/" + pafwadi.get('raw',"")
+binoho = patoho + "/" + pafwadi.get('trimmomatic',"")
+resuho = patoho + "/" + pafwadi.get('trimmoresut',"")
 
-# pafwa- : related to the path/pathway
-patoho = configdi.get('path',{}).get('pwd',"")
-# binoho: <bin>
-binoho = configdi.get('path',{}).get('trimmomatic',"")
-# resuho: path of output directory
-resuho = configdi.get('path',{}).get('trimmoresut',"")
-# tredasi: threads
+tribeli = configdi.get('tribe',[])
+fallidi = configdi.get('file-list',{})
 tredasi = configdi.get('thread',"")
 
 # linose: first part of command
-linoli = ['java','-jar', binoho+'trimmomatic-0.35.jar','PE',
-'-phred33','-threads',tredasi]
+linoli = [ 'java', '-jar', binoho+'trimmomatic-0.35.jar', 'PE',
+'-phred33', '-threads', tredasi, '-trimlog']
+
+linali = [ "ILLUMINACLIP:TruSeq3-PE.fa:2:30:10", "LEADING:3",
+"TRAILING:3", "SLIDINGWINDOW:4:15", "MINLEN:36"]
 
 if len(sys.argv) == 1:
     print(helber)
 
 # --- Main ---
+for seta in sys.argv:
+    if seta in tribeli:
+        fallisi = fallidi.get(fallisi,{})
+        for lino in open(fallisi).read().splitlines():
+            linuli = []
+            inputsi = soroho + "/" + lino
+            inputsi = soroho + "/" + lino
+
+
+
+    elif seta in ['-h','--help','-help','/?']:
+        print(helber)
+
+    else:
+        print( 'Ignore: ' + seta )
