65ee18f07742c8ecd620f5ea64bc6fa7a212d42e
David Soh <sotong0221@gmail.com>
2018-04-26 13:30:42 +0800

    finish second part and add png output

--- exp06-fastqc-summary-polymerize.py
+++ exp06-fastqc-summary-polymerize.py
@@ -2,24 +2,31 @@ import json, pprint
 
 configsi = 'data/config.json'
 
-configfi = open(configsi,'r')
+configfa = open(configsi,'r')
 
-configdi = json.load(configfi).get('var',{})
+configso = json.load(configfa)
+configdi = configso.get('var',{})
 pafwasi = configdi.get('path',{}).get('fastqc-comb',"")
 grupoli = configdi.get('group',[])
 tribeli = configdi.get('tribe',[])
 prefisdi = configdi.get('prefix',{})
 blanksi = configdi.get('blank-prefix',{})
 
-faslidi = json.load(configfi).get('fastqc-list',{})
-rowlidi = faslidi.get("row",{})
-colspan = faslidi.get("colspan",{})
+pngconfi = configdi.get('png',0)
+pnglibfa = open('exp06-fsp-pngtemplate.json','r')
+pnglibdi = json.load(pnglibfa)
+
+faslidi = configso.get('fastqc-list',{})
+rowlist = faslidi.get("row",{})
+rowlitu = tuple(rowlist)
+colspan = faslidi.get("colspan",1)
 
 inputsi = pafwasi + '/result.json'
 inputfa = open(inputsi,'r')
-inputli = json.load(inputfa)
+inputso = json.load(inputfa)
 
-faledi = inputli.get('fale',{})
+faledi = inputso.get('fale',{})
+catedi = inputso.get('cate',{})
 
 resutdi = {}
 resutsi = pafwasi + '/result.html'
@@ -27,18 +34,18 @@ resutfa = open(resutsi,'w')
 
 """
 resutdi={
-group:{ # primadi
-tribe:[ # seconli / blankli
-fale,
-fale
-]
-},
-group:{
-tribe:[
-fale,
-fale
-]
-}
+    group:{ # primadi
+        tribe:[ # seconli / blankli
+            fale,
+            fale
+        ]
+    },
+    group:{
+        tribe:[
+            fale,
+            fale
+        ]
+    }
 }
 """
 
@@ -69,5 +76,100 @@ for grupo in grupoli:
     resutdi.update({ grupo : primadi })
 
 # pprint.pprint(resutdi)
+
+headsi = """<!DOCTYPE html>
+<html>
+<head>
+<style>
+h2 {
+    text-align: center
+}
+table, th, td {
+    border: 2px solid black;
+    border-collapse: collapse;
+    background-color: #EDF7F9;
+    margin: auto;
+    text-align: center;
+}
+th, td {
+    padding: 15px;
+}
+</style>
+</head>
+<body>
+"""
+resutfa.write(headsi)
+
+for grupo in list(resutdi.keys()):
+
+    metasi = ""
+    metasi = "<table>\n    <caption><h2>" + grupo + "</h2></caption>\n"
+    resutfa.write(metasi)
+
+    primadi = {}
+    primadi = resutdi.get(grupo,{})
+
+    tribetu = ()
+    tribetu = tuple(primadi.keys())
+    faletu = ()
+
+    metasi = "    <tr>\n        <th>Types</th>\n"
+    resutfa.write(metasi)
+
+    for n in range(len(tribetu)):
+        tribe = ""
+        tribe = tribetu[n]
+        if colspan > 1:
+            metasi = "        <th colspan=\"" + str(colspan) + "\">" + tribe + "</th>\n"
+        else:
+            metasi = "        <th>" + tribe + "</th>\n"
+        resutfa.write(metasi)
+
+        seconli = primadi.get(tribe,[])
+        faletu = faletu + tuple(sorted(seconli))
+
+    metasi = "    </tr>\n"
+    resutfa.write(metasi)
+
+    metasi = "    <tr>\n        <th>Files</th>\n"
+    resutfa.write(metasi)
+
+    for n in range(len(faletu)):
+        fale = ""
+        fale = faletu[n]
+        metasi = "        <th>" + fale + "</th>\n"
+        resutfa.write(metasi)
+
+    metasi = "    </tr>\n"
+    resutfa.write(metasi)
+
+    for nume in range(len(rowlitu)):
+        rowlisi=""
+        rowlisi = rowlitu[nume]
+        metasi = "    <tr>\n        <th>" + rowlisi + "</th>\n"
+        resutfa.write(metasi)
+
+        for n in range(len(faletu)):
+            fale = ""
+            fale = faletu[n]
+            valusi = catedi.get(rowlisi,{}).get(fale,'')
+            if pngconfi == 1:
+                metasi = "        <td>" + pnglibdi.get(valusi,"") + "</td>\n"
+            else:
+                metasi = "        <td>" + valusi + "</td>\n"
+            resutfa.write(metasi)
+
+
+        metasi = "    </tr>\n"
+        resutfa.write(metasi)
+
+    metasi = "</table>\n<br>"
+    resutfa.write(metasi)
+
+
+endsi = """</body>
+</html>
+"""
+resutfa.write(endsi)
 
 resutfa.close()
