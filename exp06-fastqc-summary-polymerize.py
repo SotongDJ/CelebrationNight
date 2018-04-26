98ff615304aedd6a86776491e7d6755de1f05c62
David Soh <sotong0221@gmail.com>
2018-04-26 12:16:44 +0800

    finish sorting

--- exp06-fastqc-summary-polymerize.py
+++ exp06-fastqc-summary-polymerize.py
@@ -3,12 +3,17 @@ import json, pprint
 configsi = 'data/config.json'
 
 configfi = open(configsi,'r')
-configdi = json.load(configfi).get('var',{})
 
+configdi = json.load(configfi).get('var',{})
 pafwasi = configdi.get('path',{}).get('fastqc-comb',"")
+grupoli = configdi.get('group',[])
 tribeli = configdi.get('tribe',[])
 prefisdi = configdi.get('prefix',{})
-grupoli = configdi.get('group',[])
+blanksi = configdi.get('blank-prefix',{})
+
+faslidi = json.load(configfi).get('fastqc-list',{})
+rowlidi = faslidi.get("row",{})
+colspan = faslidi.get("colspan",{})
 
 inputsi = pafwasi + '/result.json'
 inputfa = open(inputsi,'r')
@@ -20,17 +25,49 @@ resutdi = {}
 resutsi = pafwasi + '/result.html'
 resutfa = open(resutsi,'w')
 
+"""
 resutdi={
-    group:{
-        tribe:[
-            fale,
-            fale
-        ]
-    },
-    group:{
-        tribe:[
-            fale,
-            fale
-        ]
-    }
+group:{ # primadi
+tribe:[ # seconli / blankli
+fale,
+fale
+]
+},
+group:{
+tribe:[
+fale,
+fale
+]
 }
+}
+"""
+
+for grupo in grupoli:
+    primadi = {}
+    primadi = resutdi.get(grupo,{})
+
+    for tribe in tribeli:
+        seconli = {}
+        seconli = primadi.get(tribe,[])
+
+        blankli = {}
+        blankli = primadi.get(blanksi,[])
+
+        for fale in faledi:
+            if grupo in fale:
+
+                prefis = prefisdi.get(tribe,"")
+                if prefis != "":
+                    if prefis in fale:
+                        seconli.append(fale)
+                    else:
+                        blankli.append(fale)
+
+        primadi.update({ tribe : seconli })
+        primadi.update({ blanksi : blankli })
+
+    resutdi.update({ grupo : primadi })
+
+# pprint.pprint(resutdi)
+
+resutfa.close()
