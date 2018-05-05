8b18426a2649b2ee4f7e3bc7f5ebdb104c07fe6b
David Soh <sotong0221@gmail.com>
2018-05-06 05:28:03 +0800

    fninish writing

--- librun.py
+++ librun.py
@@ -1,12 +1,6 @@
 import pprint, time
 from subprocess import call
 
-def datuse(numein=0):
-    if len(str(numein)) < 2:
-        return '0'+str(numein)
-    else:
-        return str(numein)
-
 def maxlen(lisli=['la','fafa']):
     metali = []
     for namasi in lisli:
@@ -31,10 +25,42 @@ def timme(timasi="",sepere=""):
 
     return timaki
 
-def calti(begi='0', endi='360', modde=1):
-    numein = int(endi) - int(begi)
-    numesi = str(numein)
-    return numesi
+def printe(frasi="",logisi=""):
+    if frasi != "":
+        if logisi != "":
+            print(frasi)
+            with open(logisi,'a') as logifa:
+                logifa.write(frasi+"\n")
+
+def calti(begisi="20200202020202"):
+    timasi = time.strftime("%Y%m%d%H%M%S")
+    yedifi = int(timasi[0:4])-int(begisi[0:4])
+    modifi = int(timasi[4:6])-int(begisi[4:6])
+    dadifi = int(timasi[6:8])-int(begisi[6:8])
+    hodifi = int(timasi[8:10])-int(begisi[8:10])
+    midifi = int(timasi[10:12])-int(begisi[10:12])
+    sedifi = int(timasi[12:14])-int(begisi[12:14])
+
+    if sedifi < 0 :
+        midifi = midifi -1
+        sedifi = sedifi + 60
+    if midifi < 0 :
+        hodifi = hodifi -1
+        midifi = midifi + 60
+    if hodifi < 0 :
+        dadifi = dadifi -1
+        hodifi = hodifi + 24
+
+    if dadifi < 0 :
+        resut = "More than one month..."
+    else:
+        resut = (
+            str(hodifi) + " hr "+
+            str(midifi) + " min "+
+            str(sedifi) + " s "
+        )
+
+    return resut
 
 def hedda(filasi='',locadi={},libadi={},prelogi=""):
     if filasi == '':
@@ -50,8 +76,10 @@ def hedda(filasi='',locadi={},libadi={},prelogi=""):
         metain = maxlen(metali)
         for loca in metali:
             if len(loca) < metain :
-                scriptlog = (scriptlog + loca + ' '*(metain-len(loca)) + ": " +
-                    pprint.pformat(locadi.get(loca)) + "\n")
+                loca = loca + ' '*(metain-len(loca))
+
+            scriptlog = (scriptlog + loca + ": " +
+                pprint.pformat(locadi.get(loca)) + "\n")
 
     else:
         scriptlog = ""
@@ -63,9 +91,12 @@ def hedda(filasi='',locadi={},libadi={},prelogi=""):
         metain = maxlen(metali)
         for liba in metali:
             if len(loca) < metain :
-                configlog = (configlog + "\"" + liba + "\"" +
-                    ' '*(metain-len(liba)) + ": " +
-                    pprint.pformat(libadi.get(liba)) + "\n")
+                liba = "\"" + liba + "\"" + ' '*(metain-len(liba))
+            else:
+                liba = "\"" + liba + "\""
+
+            configlog = (configlog + liba + ": " +
+                pprint.pformat(libadi.get(liba)) + "\n")
 
     else:
         configlog = ""
@@ -77,7 +108,7 @@ def hedda(filasi='',locadi={},libadi={},prelogi=""):
 
     print(runninlog + scriptlog + "\n" + configlog)
     with open(logisi,"w") as logifa:
-        logifa.write(runninlog + scriptlog + "\n" + configlog)
+        logifa.write(runninlog + scriptlog + "\n" + configlog + "\n")
 
     return timani,logisi
 
@@ -89,10 +120,11 @@ def ranni(comali=[], logini='', modde='a'):
 
     if comali == []:
         comman = ['echo','wahaha']
+    else:
+        comman = comali
 
     if logini == '':
         timani = time.strftime("%Y%m%d%H%M%S")
-        timme(modde=2)
         modden = 'a'
         logisi = "temp/temp-" + timme(timasi=timani,sepere="-_-") + '.log'
     else:
@@ -110,6 +142,6 @@ def ranni(comali=[], logini='', modde='a'):
     print(runisi + comasi)
 
     with open(logisi,modden) as logifa:
-        logifa.write(runisi + comasi)
+        logifa.write(runisi + comasi + "\n")
 
     call(comman, stdout=open(logisi, 'a'))
