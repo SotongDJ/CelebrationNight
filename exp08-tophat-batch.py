352a26df48997770c6f6de91cfd5c78bbbaf741d
David Soh <sotong0221@gmail.com>
2018-05-10 12:42:12 +0800

    exp08: finish

--- exp08-tophat-batch.py
+++ exp08-tophat-batch.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-import sys, json, pprint, time
-from subprocess import call
-
+import librun, libconfig, sys
 helber="""
    --- README of exp08-tophat-batch ---
- Title:
-    Batch Processing for TopHat
+  Title:
+    Batch Processing for TopHat2
 
- Usage:
-    python exp08-tophat-batch <TRIBE> <GROUP,GROUP,GROUP...>
+  Usage:
+    python exp08-tophat-batch -t <TRIBE> -g <GROUP,GROUP,GROUP...>
 
- Structure:
+  Data Structure:
     First : tribe,
         e.g. raw, untrim, trimmed...
     Second: group,
@@ -20,9 +18,14 @@ helber="""
 
     Visualise graph: explanation01-dataStructure.svg
 
- Original command:
-
- CAUTION:
+  Original command:
+  tophat2
+        -p [4]
+        -o [output folder]
+        [prefix of bowtie2-build genome index]
+        [forward fastq files]
+        [reverse fastq files]
+  CAUTION:
     <GROUP> must separate with comma
     <GROUP> don't allowed spacing
 
@@ -40,3 +43,68 @@ helber="""
   -fa: File (with open())
   -so: JSON
 """
+Confi = libconfig.confi()
+class loggo(librun.loggi):
+    def pesonai(self):
+        self.dicodi = {
+            "tribe"   : [],
+            "group"   : [],
+        }
+        self.sync()
+        self.adcoli = ["tophat2"]
+        self.becoli = ["-p",Confi.get("run/thread")]
+        self.cecoli = ["-o"]
+        self.edcoli = [Confi.getli(["result/bowtie", "idx/genome"])]
+
+        self.libadi = {}
+
+        self.filasi = "exp08-tophat-batch.py"
+
+        self.comali = []
+
+        self.prelogi = "data/99-log/exp08-tophat-batch-"
+
+Runni = loggo()
+
+tibeli = Runni.dicodi.get("tribe",[])
+gupoli = Runni.dicodi.get("group",[])
+Runni.hedda()
+for tibe in tibeli:
+    for gupo in gupoli:
+        Runni.comali = []
+        Runni.comali.extend(Runni.adcoli)
+        Runni.comali.extend(Runni.becoli)
+        Runni.comali.extend(Runni.cecoli)
+
+        decoli = [
+            Confi.get("result/tophat") + "/" +
+            Confi.get("data/prefix/"+tibe) + "-" + gupo
+        ]
+        Runni.comali.extend(decoli)
+
+        Runni.comali.extend(Runni.edcoli)
+
+        fecoli = [
+            Confi.get("result/raw") + "/" +
+            tibe + "/" +
+            Confi.get("data/prefix/"+tibe) + "-" +
+            gupo + "-" +
+            Confi.get("string/forward") + ".fastq"
+        ]
+        Runni.comali.extend(fecoli)
+
+        gecoli = [
+            Confi.get("result/raw") + "/" +
+            tibe + "/" +
+            Confi.get("data/prefix/"+tibe) + "-" +
+            gupo + "-" +
+            Confi.get("string/reverse") + ".fastq"
+        ]
+        Runni.comali.extend(gecoli)
+
+        # Runni.frasi = " ".join(Runni.comali)
+        # Runni.logisi = "temp/temp.log"
+        # Runni.printe()
+
+        Runni.ranni()
+Runni.calti()
