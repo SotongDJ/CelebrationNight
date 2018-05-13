fc22550920c16688e05ae8d0e451b37ae40ed73f
David Soh <sotong0221@gmail.com>
2018-05-14 07:26:11 +0800

    exp09: change config key names

--- exp09-hisat-batch.py
+++ exp09-hisat-batch.py
@@ -113,7 +113,7 @@ for tibe in tibeli:
                 tibe + "/" +
                 Confi.get("data/prefix/"+tibe) + "-" +
                 gupo + "-" +
-                Confi.get("string/forward") + ".fastq"
+                Confi.get("string/postfix/forward") + ".fastq"
             ]
 
             Runni.comali.extend(fecoli)
@@ -124,7 +124,7 @@ for tibe in tibeli:
                 tibe + "/" +
                 Confi.get("data/prefix/"+tibe) + "-" +
                 gupo + "-" +
-                Confi.get("string/reverse") + ".fastq"
+                Confi.get("string/postfix/reverse") + ".fastq"
             ]
 
             Runni.comali.extend(hecoli)
