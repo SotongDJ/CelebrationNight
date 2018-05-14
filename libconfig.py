66c5e6f45c9e26add9e146113d588eae1a3f8ab0
David Soh <sotong0221@gmail.com>
2018-05-14 08:28:36 +0800

    libconfig: add refreshing function for re-arrangement of config.json

--- libconfig.py
+++ libconfig.py
@@ -4,20 +4,28 @@ class confi:
         configfi = "data/config.json"
         configfa = open(configfi,'r')
         self.confi = json.load(configfa)
+
     def get(self,wodsi):
         resut = self.confi.get(wodsi)
         return resut
+
     def getpaf(self,wodli):
         resut = []
         for wodsi in wodli:
             resut.append(self.confi.get(wodsi,""))
         return "/".join(resut)
+
     def check(self,wodsi):
         resut = False
         metali = list(self.confi.keys())
         if wodsi in metali:
             resut = True
         return resut
+
+    def refes(self):
+        configfi = "data/config.json"
+        with open(configfi,'w') as configfa:
+            json.dump(self.confi,configfa,indent=4,sort_keys=True)
 
     def __init__(self):
         self.loadso()
