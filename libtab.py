bf4e8f0ed1ab3eda09f0887d41818c9572f00974
David Soh <sotong0221@gmail.com>
2018-05-21 22:25:52 +0800

    libtab: import database.py into libtab.tab2json

--- libtab.py
+++ libtab.py
@@ -2,7 +2,81 @@
 import sys, pprint, json
 import librun
 
-class runno(librun.loggi):
+class tab2json(librun.loggi):
+
+    def pesonai(self):
+        # self.testing = True
+
+        self.dicodi = {
+            "files" : [],
+            "id" : ""
+        }
+        self.sync()
+
+        self.tagesi = ""
+
+        self.comali=[]
+
+        self.filasi = "libtab.py"
+        self.libadi = {}
+        self.prelogi = "tmp/tmp-"
+
+    def actor(self):
+        soceli = self.dicodi.get("files",[])
+        tagesi = self.dicodi.get("id","")
+        print((soceli,tagesi))
+
+        for socesi in soceli:
+            print(socesi)
+            lineli = open(socesi).read().splitlines()
+            adnubo = True
+            namedi = {}
+
+            resudi = {}
+            remadi = {}
+
+            for line in lineli:
+                if adnubo:
+                    metali = line.split("	")
+                    for n in range(len(metali)):
+                        if metali[n] not in namedi.values():
+                            namedi.update({ n : metali[n] })
+                        if metali[n] not in remadi.keys():
+                            remadi.update({ metali[n] :{} })
+                    adnubo = False
+                else:
+                    metali = line.split("	")
+                    metadi = {}
+                    idisi = ""
+
+                    for n in range(len(list(namedi.keys()))):
+                        colusi = namedi.get(n)
+                        metadi.update({ colusi : metali[n]})
+
+                        if colusi == tagesi:
+                            idisi = metali[n]
+
+                    resudi.update({ idisi : metadi })
+
+                    for n in range(len(list(namedi.keys()))):
+                        colusi = namedi.get(n)
+
+                        almedi = remadi.get(colusi,{})
+                        almeli = almedi.get(metali[n],[])
+
+                        almeli.append(idisi)
+
+                        almedi.update({ metali[n] : almeli })
+                        remadi.update({ colusi : almedi })
+
+
+            with open(socesi.split(".")[0]+".json","w") as resufi:
+                json.dump(resudi,resufi,indent=4,sort_keys=True)
+
+            with open(socesi.split(".")[0]+"-column.json","w") as resufi:
+                json.dump(remadi,resufi,indent=4,sort_keys=True)
+
+class json2tab(librun.loggi):
 
     def pesonai(self):
         # self.testing = True
