5be300823dc94bb2c20211ad99e38707420a3f9e
David Soh <sotong0221@gmail.com>
2018-05-21 22:59:21 +0800

    library: add header

--- libconfig.py
+++ libconfig.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import json
 class confi:
     def loadso(self):
--- librun.py
+++ librun.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import pprint, time, json, sys
 import libconfig
 from subprocess import call
