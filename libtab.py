65dfaae780783f20182d69717d7d64c67a0df874
David Soh <sotong0221@gmail.com>
2018-05-21 17:49:43 +0800

    libtab: import from db01 project

---
+++ libtab.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import sys, pprint, json
+import librun
+
+class runno(librun.loggi):
+
+    def pesonai(self):
+        # self.testing = True
+        self.typesi = 'library'
+
+        self.dicodi = {
+            "files" : []
+        }
+        self.sync()
+
+        self.tagesi = ""
+
+        self.comali=[]
+
+        self.filasi = "libtab.py"
+        self.libadi = {}
+        self.prelogi = "tmp/tmp-"
+
+    def actor(self):
+        soceli = self.dicodi.get("files",[])
+        print(soceli)
+
+        for socesi in soceli:
+            socefi = open(socesi,'r')
+            soceso = json.load(socefi)
+
+            coluse = set()
+            metadi = dict()
+            for id in list(soceso.keys()):
+                metadi = soceso.get(id,{})
+                coluse.update(set(metadi.keys()))
+
+            colutu = tuple(sorted(coluse))
+            with open(socesi.replace(".json",".tsv"),"w") as resufi:
+                resufi.write("id"+"	"+"	".join(colutu)+"\n")
+                for id in list(soceso.keys()):
+                    line = id
+                    metadi = dict()
+                    metadi = soceso.get(id,{})
+                    for meta in colutu:
+                        line = line + "	" + metadi.get(meta,"")
+                    resufi.write(line+"\n")
+Ano = runno()
