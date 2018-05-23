c454c040408c15ce65851cab04ecce663fccef74
David Soh <sotong0221@gmail.com>
2018-05-24 01:50:09 +0800

    libsnm: start development. This library is for exp15

---
+++ libsnm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys, pprint, json
+import librun
+global helber
+helber="""
+   --- README of library-search-and-merge ---
+  Title:
+    Searching and Pairing Tool for Transcript Info. and StringTie Result
+
+  Usage:
+    import libsnm
+
+    
+   --- README ---
+""""""
+ Postfix of variables:
+  -si: String
+   -ni: alternative/second string for same Usage
+   -fi: string for open()
+  -ho: String(that store dir path)
+  -ti: Intiger/Float
+  -li: List
+  -tu: Tuple
+  -di: Dictionary
+  -fa: File (with open())
+  -so: JSON
+"""
+class annot(librun.loggi):
