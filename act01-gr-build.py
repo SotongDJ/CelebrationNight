d53c54834b38c72e99ad704dd05789d43dec34bb
David Soh <sotong0221@gmail.com>
2018-07-09 11:30:28 +0800

    act01: adopt new libconfig structure and new naming style

--- act01-gr-build.py
+++ act01-gr-build.py
@@ -26,48 +26,84 @@ class genomerefer(libWorkFlow.workflow):
         self.helper_msg_str = helper_msg_block
 
         self.requested_argv_dict = {
-            "refer" : "",
+            "refer": "",
+            "genome": "",
+            "annotation": ""
         }
         self.SynonymDict.input(ConfigDict.get_dict("synom"))
         self.synchornize()
 
         self.comand_line_list = []
         self.script_name_str = "act01-Genome-Reference-build.py"
-        self.requested_config_dict = {
-            "bin/hisat2build" : ConfigDict.get_str("bin/hisat2build"),
-            "index/hisat"     : ConfigDict.get_str("index/hisat"),
-            "result/log"      : ConfigDict.get_str("result/log"),
-            "run/thread"      : ConfigDict.get_str("run/thread"),
-            "refer/annotate"  : ConfigDict.get_dict("refer/annotate"),
+        ConfigDict.requested_dict = {}
+        ConfigDict.requested_dict = {
+            "bin/hisat2build": "",
+            "result/log": "",
+            "index/hisat": "",
+            "run/thread": "",
+            "refer/annotate": {},
+            "refer/genome": {},
         }
+        self.requested_config_dict = ConfigDict.get_batchly()
         self.log_file_prefix_str = self.requested_config_dict.get("result/log")+"/act01-gr-build-"
 
     def actor(self):
-        self.genome_file_path = self.requested_argv_dict.get("genome","")
-        self.prefix_str = self.requested_argv_dict.get("prefix" ,"")
-        self.tribe_str = self.requested_argv_dict.get("tribe" ,"")
+        self.refer_codename_str = self.requested_argv_dict.get("refer","")
+        self.genome_path = self.requested_argv_dict.get("genome" ,"")
+        self.annotation_path = self.requested_argv_dict.get("annotation" ,"")
 
         self.startLog()
 
-        self.phrase_str = "==========\nStage 1 : Build HISAT2 Index\n=========="
-        self.printPhrase()
+        genome_boolean = False
+        annotation_boolean = False
+        if self.refer_codename_str != "":
+            genome_temp_dict = self.requested_config_dict.get("refer/genome",{})
+            genome_temp_path = genome_temp_dict.get(self.refer_codename_str,"")
+            if self.genome_path == "" and genome_temp_path != "":
+                self.genome_path = genome_temp_path
+                genome_boolean = True
+            elif self.genome_path != "":
+                genome_temp_dict.update({ self.refer_codename_str : self.genome_path })
+                ConfigDict.update({ "refer/genome" : genome_temp_dict })
+                genome_boolean = True
 
-        self.binary_path = self.requested_config_dict.get("bin/hisat2build")
-        self.thread_argv_list = ["-p",self.requested_config_dict.get("run/thread")]
-        self.output_path = self.requested_config_dict.get("index/hisat") + "/" + self.prefix_str
+            annotation_temp_dict = self.requested_config_dict.get("refer/annotate",{})
+            annotation_temp_path = annotation_temp_dict.get(self.refer_codename_str,"")
+            if self.annotation_path == "" and annotation_temp_path != "":
+                self.annotation_path = annotation_temp_path
+                annotation_boolean = True
+            elif self.annotation_path != "":
+                annotation_temp_dict.update({ self.refer_codename_str : self.annotation_path })
+                ConfigDict.update({ "refer/annotate" : annotation_temp_dict })
+                annotation_boolean = True
 
-        self.comand_line_list = []
-        self.comand_line_list.append(self.binary_path)
-        self.comand_line_list.extend(self.thread_argv_list)
-        self.comand_line_list.append(self.genome_file_path)
-        self.comand_line_list.append(self.output_path)
-        self.runCommand()
+        if self.refer_codename_str != "" and genome_boolean and annotation_boolean:
+            self.phrase_str = "==========\nStage 1 : Build HISAT2 Index\n=========="
+            self.printPhrase()
 
-        self.phrase_str = "==========\nStage 2 : Copy Genome FASTA to HISAT2 Index folder\n=========="
-        self.printPhrase()
+            self.binary_path = self.requested_config_dict.get("bin/hisat2build")
+            self.thread_argv_list = ["-p",self.requested_config_dict.get("run/thread")]
+            self.output_path = self.requested_config_dict.get("index/hisat") + "/" + self.refer_codename_str
 
-        self.comand_line_list = ["cp",self.genome_file_path,self.output_path+".fa"]
-        self.runCommand()
+            self.comand_line_list = []
+            self.comand_line_list.append(self.binary_path)
+            self.comand_line_list.extend(self.thread_argv_list)
+            self.comand_line_list.append(self.genome_path)
+            self.comand_line_list.append(self.output_path)
+            self.runCommand()
+
+            self.phrase_str = "==========\nStage 2 : Copy Genome FASTA to HISAT2 Index folder\n=========="
+            self.printPhrase()
+
+            self.comand_line_list = ["cp",self.genome_path,self.output_path+".fa"]
+            self.runCommand()
+
+        else:
+            self.phrase_str = (
+                "Required codename of [refer], \n"
+                +"use \"--help\" argument for further info."
+            )
+            self.printPhrase()
 
         self.phrase_str = "==========\nStage 3 : Extract GFF Information\n=========="
         self.printPhrase()
@@ -77,13 +113,14 @@ class genomerefer(libWorkFlow.workflow):
         Gekta.testing = self.testing
         Gekta.log_file_prefix_str = self.requested_config_dict.get("result/log")+"/act01-grb-libgext-"
         Gekta.requested_argv_dict = {
-            "input"  : [self.requested_config_dict.get("refer/annotate").get(self.tribe_str)],
+            "input"  : [annotation_path],
             "tribe"  : self.tribe_str,
-            "output" : self.prefix_str+"-gff-info.json"
+            "output" : self.refer_codename_str+"-gff-info.json"
         }
         Gekta.actor()
         """
         self.printBlankLine()
+
 
         self.stopLog()
 
