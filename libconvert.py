27dd72059819d6089e595e259c7f01245fea5e1a
David Soh <sotong0221@gmail.com>
2018-07-11 10:37:00 +0800

    libConvert: replace "tab" with "delimiter" and revamp the logic

--- libconvert.py
+++ libconvert.py
@@ -10,7 +10,7 @@ helper_msg_block="""
   Usage:
     import libconvert
 
-    CvtoJSON = libconvert.cvtTABtoJSON()
+    CvtoJSON = libConvert.cvtDSVtoJSON()
     CvtoJSON.requested_argv_dict = {
         "files" : [<INPUT>,<INPUT>......] ,
         "id" : "" ,
@@ -18,7 +18,7 @@ helper_msg_block="""
     }
     CvtoJSON.actor()
 
-    CvtoTAB = libconvert.cvtJSONtoTAB()
+    CvtoTAB = libConvert.cvtJSONtoDSV()
     CvtoTAB.requested_argv_dict = {
         "files" : [<INPUT>,<INPUT>......] ,
         "column": [<NAME>,<NAME>......] # for sorting
@@ -40,17 +40,19 @@ helper_msg_block="""
   -fa: File (with open())
   -so: JSON
 """
-class cvtTABtoJSON(libWorkFlow.workflow):
+class cvtDSVtoJSON(libWorkFlow.workflow):
     def redirecting(self):
         """"""
     def personalize(self):
         # self.testing = True
 
         self.requested_argv_dict = {
-            "files"  : [],
-            "id"     : "",
-            "prefix" : "",
-            "column" : ""
+            "files": [],
+            "refer_column": "",
+            "prefix": "",
+            "header": [],
+            "headless": True,
+            "delimiter": "\t"
         }
         self.synchornize()
 
@@ -58,21 +60,18 @@ class cvtTABtoJSON(libWorkFlow.workflow):
 
         self.comand_line_list=[]
 
-        self.script_name_str = "libconvert.py"
+        self.script_name = "libConvert.py"
         self.requested_config_dict = {}
         self.log_file_prefix_str = "temp/tmp-"
 
     def actor(self):
-        source_files_list = self.requested_argv_dict.get("files" ,[])
-        column_id_name = self.requested_argv_dict.get("id"    ,"")
+        source_files_list = self.requested_argv_dict.get("files",[])
+        refer_column_name = self.requested_argv_dict.get("refer_column","")
         prefix_str = self.requested_argv_dict.get("prefix","")
-        column_name_str = self.requested_argv_dict.get("column","")
+        header_list = self.requested_argv_dict.get("header",[])
+        headless_boolean = self.requested_argv_dict.get("headless",True)
+        delimiter_str = self.requested_argv_dict.get("delimiter","\t")
         self.startLog()
-
-        self.phrase_str = pprint.pformat((
-            source_files_list,column_id_name,prefix_str,column_name_str
-        ))
-        self.printTimeStamp()
 
         for source_file_name in source_files_list:
             self.phrase_str = pprint.pformat(source_file_name)
@@ -90,66 +89,70 @@ class cvtTABtoJSON(libWorkFlow.workflow):
 
             if not result_file_boolean or not column_file_boolean :
                 lines_list = open(source_file_name).read().splitlines()
+                first_line_str = lines_list[0]
 
-                name_dict = {}
+                position_x_column_dict = {}
 
                 id_x_value_dict = {}
                 column_x_value_dict = {}
 
-                if column_name_str != "":
-                    column_temp_list = column_name_str.split("	")
-                    for number in range(len(metali)):
-                        if column_temp_list[number] not in name_dict.values():
-                            name_dict.update({ number : column_temp_list[number] })
-                        if column_temp_list[number] not in column_x_value_dict.keys():
-                            column_x_value_dict.update({ column_temp_list[number] :{} })
-                    header_boolean = False
+                if not headless_boolean:
+                    if header_list == []:
+                        header_list = first_line_str.split(delimiter_str)
                 else:
-                    header_boolean = True
+                    column_temp_list = first_line_str.split(delimiter_str)
+                    max_digit_num = len(str(len(column_temp_list)))
+                    for column_num in range(len(column_temp_list)):
+                        digit_num = len(str(len(column_num)))
+                        if digit_num != max_digit_num:
+                            diff_digit_num = max_digit_num - digit_num
+                        else:
+                            diff_digit_num = 0
 
-                if column_id_name != "":
-                    column_id_exist_boolean = True
-                else:
-                    column_id_exist_boolean = False
+                        header_list.append(
+                            "Column_"+("0"*diff_digit_num)+str(column_num)
+                        )
+
+                for number in range(len(header_list)):
+                    if header_list[number] not in position_x_column_dict.values():
+                        position_x_column_dict.update({ number : header_list[number] })
+                    if header_list[number] not in column_x_value_dict.keys():
+                        column_x_value_dict.update({ header_list[number] :{} })
+
+                refer_column_exist_boolean = False
+                if refer_column_name != "":
+                    refer_column_exist_boolean = True
 
                 line_id_num = 0
                 for line_str in lines_list:
-                    if header_boolean:
-                        column_temp_list = line_str.split("	")
-                        for column_num in range(len(column_temp_list)):
-                            if column_temp_list[column_num] not in name_dict.values():
-                                name_dict.update({ column_num : column_temp_list[column_num] })
-                            if column_temp_list[column_num] not in column_x_value_dict.keys():
-                                column_x_value_dict.update({ column_temp_list[column_num] :{} })
-                        header_boolean = False
-                    elif "#" not in line_str:
-                        value_temp_list = line_str.split("	")
+                    if "#" not in line_str:
+                        value_temp_list = line_str.split(delimiter_str)
                         value_temp_dict = {}
-                        id_name_str = ""
+                        id_name = ""
 
-                        if not column_id_exist_boolean:
+                        if not refer_column_exist_boolean:
                             line_id_num = line_id_num + 1
-                            id_name_str = prefix_str + str(line_id_num)
+                            id_name = prefix_str + str(line_id_num)
 
-                        for number in range(len(list(name_dict.keys()))):
-                            column_name_str = name_dict.get(number)
-                            value_temp_dict.update({ column_name_str : value_temp_list[number]})
+                        for number in range(len(list(position_x_column_dict.keys()))):
+                            header_list_str = position_x_column_dict.get(number)
+                            value_temp_dict.update({ header_list_str : value_temp_list[number]})
 
-                            if column_name_str == column_id_name and column_id_exist_boolean:
-                                id_name_str = value_temp_list[number]
+                            if header_list_str == refer_column_name and refer_column_exist_boolean:
+                                id_name = value_temp_list[number]
 
-                        id_x_value_dict.update({ id_name_str : value_temp_dict })
+                        id_x_value_dict.update({ id_name : value_temp_dict })
 
-                        for number in range(len(list(name_dict.keys()))):
-                            column_name_str = name_dict.get(number)
+                        for number in range(len(list(position_x_column_dict.keys()))):
+                            header_list_str = position_x_column_dict.get(number)
 
-                            column_temp_dict = column_x_value_dict.get(column_name_str,{})
+                            column_temp_dict = column_x_value_dict.get(header_list_str,{})
                             id_list = column_temp_dict.get(value_temp_list[number],[])
 
-                            id_list.append(id_name_str)
+                            id_list.append(id_name)
 
                             column_temp_dict.update({ value_temp_list[number] : id_list })
-                            column_x_value_dict.update({ column_name_str : column_temp_dict })
+                            column_x_value_dict.update({ header_list_str : column_temp_dict })
 
 
                 with open(result_file_name,"w") as result_file_handle:
@@ -157,9 +160,10 @@ class cvtTABtoJSON(libWorkFlow.workflow):
 
                 with open(column_file_name,"w") as column_file_handle:
                     json.dump(column_x_value_dict,column_file_handle,indent=4,sort_keys=True)
+
         self.stopLog()
 
-class cvtJSONtoTAB(libWorkFlow.workflow):
+class cvtJSONtoDSV(libWorkFlow.workflow):
     def redirecting(self):
         """"""
 
@@ -168,7 +172,8 @@ class cvtJSONtoTAB(libWorkFlow.workflow):
 
         self.requested_argv_dict = {
             "files" : [],
-            "column" : []
+            "header" : [],
+            "delimiter": "\t"
         }
         self.synchornize()
 
@@ -176,40 +181,38 @@ class cvtJSONtoTAB(libWorkFlow.workflow):
 
         self.comand_line_list=[]
 
-        self.script_name_str = "libconvert.py"
+        self.script_name = "libConvert.py"
         self.requested_config_dict = {}
         self.log_file_prefix_str = "temp/tmp-"
 
     def actor(self):
         source_files_list = self.requested_argv_dict.get("files",[])
-        column_name_list = self.requested_argv_dict.get("column",[])
+        header_list = self.requested_argv_dict.get("header",[])
+        delimiter_str = self.requested_argv_dict.get("delimiter","\t")
         self.startLog()
-
-        self.phrase_str = pprint.pformat(source_files_list)
-        self.printTimeStamp()
 
         for source_file_name in source_files_list:
             source_file_handle = open(source_file_name,'r')
             source_json_dict = json.load(source_file_handle)
 
-            if column_name_list == []:
+            if header_list == []:
                 column_name_set = set()
                 value_temp_dict = dict()
                 for id_name in list(source_json_dict.keys()):
                     value_temp_dict = source_json_dict.get(id_name,{})
                     column_name_set.update(set(value_temp_dict.keys()))
-                column_name_tuple = tuple(sorted(column_name_set))
+                header_tuple = tuple(sorted(column_name_set))
             else:
-                column_name_tuple = tuple(column_name_list)
+                header_tuple = tuple(header_list)
 
             with open(source_file_name.replace(".json",".tsv"),"w") as result_file_handle:
-                result_file_handle.write("id"+"	"+"	".join(column_name_tuple)+"\n")
+                result_file_handle.write("id"+delimiter_str+delimiter_str.join(header_tuple)+"\n")
                 for id_name in list(source_json_dict.keys()):
                     line_str = id_name
                     value_temp_dict = dict()
                     value_temp_dict = source_json_dict.get(id_name,{})
-                    for column_name in column_name_tuple:
-                        line_str = line_str + "	" + value_temp_dict.get(column_name,"")
+                    for column_name in header_tuple:
+                        line_str = line_str + delimiter_str + value_temp_dict.get(column_name,"")
                     result_file_handle.write(line_str+"\n")
 
         self.stopLog()
@@ -223,14 +226,14 @@ class attributionExtractor(libWorkFlow.workflow):
         self.helper_msg_str = helper_msg_block
 
         self.requested_argv_dict = {
-            "input"  : [],
-            "output" : "",
-            "tribe"  : "",
+            "gff.json": [],
+            "refer": "",
         }
 
         self.comand_line_list = []
-        self.script_name_str = "libconvert.py"
+        self.script_name = "libConvert.py"
         self.requested_config_dict = {}
         self.log_file_prefix_str = "temp/tmp-"
 
-    
+    def actor(self):
+        """"""
