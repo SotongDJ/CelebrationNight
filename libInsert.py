d72658f5ac7980e07b8381447bcb3c9844b0e8ab
David Soh <sotong0221@gmail.com>
2018-07-17 09:44:07 +0800

    [libInsert] finish revamp
    - remove old description and make adjustment; 
    - simplify requested argument; 
    - adopt new libConfig structure (Branch system);

--- libInsert.py
+++ libInsert.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+import pyWorkFlow, libConfig
 import sys, pprint, json
-import pyWorkFlow
 global helper_msg_block
 helper_msg_block="""
    --- README of library-search-and-merge ---
   Title:
-    Searching and Pairing Tool for Transcript Info. and StringTie Result
+    Inserting Tool for Annotation
 
   Usage:
-    import libsnm
-    GeneID = libsnm.geneid()
+    import libInsert
+    GeneID = libInsert.geneid()
     GeneID.requested_argv_dict = {
         "description" : [file of gene description] ,
         "basement" : [result file] ,
@@ -19,81 +19,83 @@ helper_msg_block="""
         "to" : [column name of description],
     }
     # GeneID.requested_argv_dict.update()
-    GeneID.filasi = "libsnm.geneid"
+    GeneID.filasi = "libInsert.geneid"
     GeneID.actor()
 
    --- README ---
 """
-"""
- Postfix of variables:
-  -si: String
-   -ni: alternative/second string for same Usage
-   -fi: string for open()
-  -ho: String(that store dir path)
-  -ti: Intiger/Float
-  -li: List
-  -tu: Tuple
-  -di: Dictionary
-  -fa: File (with open())
-  -so: JSON
-"""
-class geneid(pyWorkFlow.workflow):
-    def redirecting(self):
-        """"""
+class inserting(pyWorkFlow.workflow):
     def personalize(self):
         # self.testing = True
+        self.type = "library"
+        self.helper_msg_str = helper_msg_block
 
         self.requested_argv_dict = {
-            "description" : "" ,
-            "basement" : "" ,
-            "if" : "",
-            "key" : "",
-            "from" : "",
-            "to" : "",
+            "branch" : "",
+            "target" : "",
+            "refer" : ""
         }
-        self.synchornize()
 
-        self.target_file_path = ""
-
+        ConfigDict = libConfig.config()
         self.comand_line_list=[]
+        ConfigDict.requested_dict = {}
+        ConfigDict.requested_dict = {
+            "result/log" : "",
+            "result/gff-json" : "",
+            "data/refer" : {},
+            "refer/database" : {},
+            "database/annotation-source" : {},
+            "database/annotation-target" : {},
+        }
+        self.requested_config_dict = ConfigDict.get_batchly()
 
         self.script_name = "library-search-and-merge"
-        self.requested_config_dict = {}
         self.log_file_prefix_str = "temp/tmp-"
 
-    def actor(self):
-        addasi = self.requested_argv_dict.get("description","")
-        socesi = self.requested_argv_dict.get("basement","")
-        ifasi = self.requested_argv_dict.get("if","")
-        keyosi = self.requested_argv_dict.get("key","")
-        frosi = self.requested_argv_dict.get("from","")
-        tonsi = self.requested_argv_dict.get("to","")
+        self.input_dict = {} # {id:{key:value}}
+        self.output_dict = {}
 
+    def actor(self):
         self.startLog()
 
-        self.frase = pprint.pformat((addasi, socesi, ifasi, keyosi, tonsi))
-        self.printTimeStamp()
+        branch_name = self.requested_argv_dict.get("branch")
+        target_name = self.requested_argv_dict.get("target")
+        refer_file_path = self.requested_argv_dict.get("refer")
 
-        addafi = open(addasi,"r")
-        addaso = json.load(addafi)
+        refer_name_dict = self.requested_config_dict.get("data/refer")
+        refer_name = refer_name_dict.get(branch_name)
 
-        socefi = open(socesi,"r")
-        soceso = json.load(socefi)
+        database_dict = self.requested_config_dict.get("refer/database")
+        db_type_name = database_dict.get(refer_name)
 
-        blanbo = False
+        annotate_source_dict = self.requested_config_dict.get("database/annotation-source")
+        source_pair_dict = annotate_source_dict.get(db_type_name)
+        source_key_str = source_pair_dict.get("key")
+        source_value_str = source_pair_dict.get("value")
 
-        if ifasi != "" and tonsi != "":
-            for id in list(soceso.keys()):
-                metadi = soceso.get(id)
-                if keyosi in metadi.get(ifasi):
-                    refesi = metadi.get(ifasi).replace(keyosi,"")
-                    resusi = addaso.get(frosi).get(refesi,"N/A")
-                    metadi.update({ tonsi : resusi })
-                    soceso.update({ id : metadi })
-                    blanbo = True
+        annotate_target_dict = self.requested_config_dict.get("database/annotation-target")
+        target_set_dict = annotate_target_dict.get(target_name+"-"+db_type_name)
+        target_key_str = target_set_dict.get("key")
+        target_value_str = target_set_dict.get("value")
+        target_replace_str = target_set_dict.get("replace")
 
-        if blanbo:
-            with open(socesi,"w") as socefi:
-                json.dump(soceso,socefi,indent=4,sort_keys=True)
+        refer_file_handle = open(refer_file_path,"r")
+        refer_file_dict = json.load(refer_file_handle)
 
+        value_temp_dict = refer_file_dict.get("{key:{value:[id]}}").get(source_key_str)
+        id_temp_dict = refer_file_dict.get("{key:{id:value}}").get(source_value_str)
+
+        if branch_name != "" and target_name != "":
+            for id in list(self.input_dict.keys()): # {id:{key:value}}
+                key_value_dict = self.input_dict.get(id)
+                value_temp_str = key_value_dict.get(target_key_str)
+                value_temp_str = value_temp_str.replace(target_replace_str,"")
+
+                convert_id_name = value_temp_dict.get(value_temp_str,"")
+                final_str = id_temp_dict.get(convert_id_name,"N/A")
+
+                key_value_dict.update({ target_value_str : final_str })
+                self.input_dict.update({ id : key_value_dict })
+
+        self.output_dict = self.input_dict
         self.stopLog()
