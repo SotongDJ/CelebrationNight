476507c666c9ff392cf04b69063cc266e680fc58
David Soh <sotong0221@gmail.com>
2019-05-07 03:49:48 +0800

    [07-df/07-sl] add actions for transcript analysis

--- 07-df1-CompareDifferences-sampleRun.py
+++ 07-df1-CompareDifferences-sampleRun.py
@@ -14,42 +14,48 @@ trim30PathStr = 'data/07-sl-expressionTable-SQLite3/{branch}/Expression-{ant}-tr
 sqlFolderStr = "data/07-df-differenceDistribution/{branch}/"
 sqlPathStr = 'data/07-df-differenceDistribution/{branch}/Difference-{ant}.db'
 
+# TPM
+
+print("-- Calculating TPM differences --")
 trim20Path = trim20PathStr.format(branch=branchStr,ant=antStr)
 Connect = sqlite3.connect(trim20Path)
-print("[SQLite3]\n    "+trim20Path)
+print("[SQLite3:Open]"+trim20Path)
 Cursor = Connect.cursor()
 
 geneidDict = dict()
+sampleExc = Cursor.execute("SELECT UUID, GeneID  from GeneExpression_{}".format(controlStr))
+for rowList in sampleExc:
+    uuid, geneid = rowList
+    geneidDict.update({ uuid : geneid })
+
 q20Dict = dict()
 for sampleStr in sampleList:
-    sampleExc = Cursor.execute("SELECT UUID, GeneID, TPM  from {}_Expression".format(sampleStr))
+    sampleExc = Cursor.execute("SELECT UUID, TPM  from GeneExpression_{}".format(sampleStr))
     for rowList in sampleExc:
-        uuid, geneid, tpm = rowList
+        uuid, tpm = rowList
         subDict = q20Dict.get(uuid,dict())
         subDict.update({ sampleStr : tpm })
         q20Dict.update({ uuid : subDict })
 
-        geneidDict.update({ uuid : geneid })
-
 Connect.close()
+print("[SQLite3:Close]"+trim20Path)
 
 trim30Path = trim30PathStr.format(branch=branchStr,ant=antStr)
 Connect = sqlite3.connect(trim30Path)
-print("[SQLite3]\n    "+trim30Path)
+print("[SQLite3:Open]"+trim30Path)
 Cursor = Connect.cursor()
 
 q30Dict = dict()
 for sampleStr in sampleList:
-    sampleExc = Cursor.execute("SELECT UUID, GeneID, TPM  from {}_Expression".format(sampleStr))
+    sampleExc = Cursor.execute("SELECT UUID, TPM  from GeneExpression_{}".format(sampleStr))
     for rowList in sampleExc:
-        uuid, geneid, tpm = rowList
+        uuid, tpm = rowList
         subDict = q30Dict.get(uuid,dict())
         subDict.update({ sampleStr : tpm })
         q30Dict.update({ uuid : subDict })
 
-        geneidDict.update({ uuid : geneid })
-
 Connect.close()
+print("[SQLite3:Close]"+trim30Path)
 
 diffrDict = dict()
 for uuid in q20Dict.keys():
@@ -85,7 +91,8 @@ for uuid in q20Dict.keys():
         subDict.update({ "GeneID" : geneidStr})
         diffrDict.update({ uuid : subDict })
 
-createComStr = "CREATE TABLE Differences_Summary ({})"
+print("\n-- Summarising of TPM differences --")
+createComStr = "CREATE TABLE TPM_DifferencesSummary ({})"
 createColumnList = [
     "'UUID'  TEXT PRIMARY KEY NOT NULL", 
     "'GeneID'  TEXT", 
@@ -116,12 +123,12 @@ for sampleStr in sampleList:
 pathlib.Path(sqlFolderStr.format(branch=branchStr)).mkdir(parents=True,exist_ok=True)
 sqlPath = sqlPathStr.format(branch=branchStr,ant=antStr)
 Connect = sqlite3.connect(sqlPath)
-print("[SQLite3]\n    "+sqlPath)
 Cursor = Connect.cursor()
 ReturnMsg = Cursor.execute(createComStr.format(",".join(createColumnList)))
 Connect.commit()
+print("[SQLite3:CreateTable] "+sqlPath)
 
-insertComStr = "INSERT INTO Differences_Summary ({column}) VALUES ({value})"
+insertComStr = "INSERT INTO TPM_DifferencesSummary ({column}) VALUES ({value})"
 a = 0
 for uuid in diffrDict.keys():
     valueList = list()
@@ -133,10 +140,13 @@ for uuid in diffrDict.keys():
     ReturnMsg = Cursor.execute(insertCommand,valueList)
 
 Connect.commit()
+print("[SQLite3:Insert] "+sqlPath)
 Connect.close()
+print("[SQLite3:Close]\n")
 
+print("\n-- Exporting of TPM differences per sample --")
 for sampleStr in sampleList:
-    sampleCreateComStr = "CREATE TABLE Differences_{} ({})"
+    sampleCreateComStr = "CREATE TABLE TPM_Differences_{} ({})"
     sampleCreateColumnList = [
         "'UUID'  TEXT PRIMARY KEY NOT NULL", 
         "'GeneID'  TEXT", 
@@ -182,20 +192,227 @@ for sampleStr in sampleList:
 
     sqlPath = sqlPathStr.format(branch=branchStr,ant=antStr)
     Connect = sqlite3.connect(sqlPath)
-    print("[SQLite3]\n    "+sqlPath)
     Cursor = Connect.cursor()
     ReturnMsg = Cursor.execute(sampleCreateComStr.format(sampleStr,",".join(sampleCreateColumnList)))
     Connect.commit()
+    print("[SQLite3:CreateTable] "+sqlPath)
 
     for uuid in diffrDict.keys():
         valueList = list()
         for posInt in range(len(sampleInsertList)):
             valueList.append(sampleDict[uuid][sampleInsertList[posInt]])
 
-        sampleInsertComStr = "INSERT INTO Differences_{sample} ({column}) VALUES ({value})"
+        sampleInsertComStr = "INSERT INTO TPM_Differences_{sample} ({column}) VALUES ({value})"
         insertCommand = sampleInsertComStr.format(sample=sampleStr, column=",".join(sampleInsertList),value=(("?,"*(len(valueList)-1)))+"?")
         ReturnMsg = Cursor.execute(insertCommand,valueList)
 
     Connect.commit()
+    print("[SQLite3:Insert] "+sqlPath)
     Connect.close()
+    print("[SQLite3:Close]\n")
+
+# FPKM
+
+print("\n-- Calculating FPKM differences --")
+trim20Path = trim20PathStr.format(branch=branchStr,ant=antStr)
+Connect = sqlite3.connect(trim20Path)
+print("[SQLite3:Open]"+trim20Path)
+Cursor = Connect.cursor()
+
+tidDict = dict()
+q20Dict = dict()
+sampleExc = Cursor.execute("SELECT UUID, TranscriptID, TranscriptName, GeneID, GeneName from TranscriptExpression_{}".format(controlStr))
+for rowList in sampleExc:
+    uuid, tID, tName, geneID, geneName = rowList
+    subDict = tidDict.get(uuid,dict())
+    subDict.update({ "TranscriptID" : tID })
+    subDict.update({ "TranscriptName" : tName })
+    subDict.update({ "GeneID" : geneID })
+    tidDict.update({ uuid : subDict })
+
+for sampleStr in sampleList:
+    sampleExc = Cursor.execute("SELECT UUID, FPKM  from TranscriptExpression_{}".format(sampleStr))
+    for rowList in sampleExc:
+        uuid, fpkm = rowList
+        subDict = q20Dict.get(uuid,dict())
+        subDict.update({ sampleStr : fpkm })
+        q20Dict.update({ uuid : subDict })
+
+Connect.close()
+print("[SQLite3:Close]"+trim20Path)
+
+trim30Path = trim30PathStr.format(branch=branchStr,ant=antStr)
+Connect = sqlite3.connect(trim30Path)
+print("[SQLite3:Open]"+trim30Path)
+Cursor = Connect.cursor()
+
+q30Dict = dict()
+for sampleStr in sampleList:
+    sampleExc = Cursor.execute("SELECT UUID, FPKM  from TranscriptExpression_{}".format(sampleStr))
+    for rowList in sampleExc:
+        uuid, fpkm = rowList
+        subDict = q30Dict.get(uuid,dict())
+        subDict.update({ sampleStr : fpkm })
+        q30Dict.update({ uuid : subDict })
+
+Connect.close()
+print("[SQLite3:Close]"+trim30Path)
+
+diffrDict = dict()
+for uuid in q20Dict.keys():
+    tNameStr = tidDict[uuid]["TranscriptName"]
+    geneidStr = tidDict[uuid]["GeneID"]
+    for sampleStr in sampleList:
+        q20Flt = q20Dict[uuid][sampleStr]
+        q30Flt = q30Dict[uuid][sampleStr]
+        diffFlt = abs(q30Flt-q20Flt)
+
+        minFlt = sorted([q20Flt,q30Flt])[0]
+        if minFlt != 0.0:
+            ratioFlt = diffFlt/minFlt
+        else:
+            ratioFlt = 0.0
         
+        if (q30Flt-q20Flt) > 0:
+            dirStr = "increase"
+        elif (q30Flt-q20Flt) < 0:
+            dirStr = "decrease"
+        else:
+            dirStr = "remain"
+        
+        if diffFlt != 0:
+            groupIntStr = str(int(math.ceil(math.log10(diffFlt))))
+        else:
+            groupIntStr = 'none'
+        
+        subDict = diffrDict.get(uuid,dict())
+        subDict.update({ "Difference_"+sampleStr : diffFlt })
+        subDict.update({ "Direction_"+sampleStr : dirStr })
+        subDict.update({ "Ratio_"+sampleStr : ratioFlt })
+        subDict.update({ "Group_"+sampleStr : groupIntStr })
+        subDict.update({ "TranscriptName" : tNameStr})
+        subDict.update({ "GeneID" : geneidStr})
+        diffrDict.update({ uuid : subDict })
+
+print("\n-- Summarising of FPKM differences --")
+createComStr = "CREATE TABLE FPKM_DifferencesSummary ({})"
+createColumnList = [
+    "'UUID'  TEXT PRIMARY KEY NOT NULL", 
+    "'TranscriptName'  TEXT", 
+    "'GeneID'  TEXT", 
+]
+columnList = ["UUID","TranscriptName","GeneID"]
+insertColumnList = ["TranscriptName","GeneID"]
+for sampleStr in sampleList:
+    columnStr = "Difference_{} REAL".format(sampleStr)
+    createColumnList.append(columnStr)
+    columnList.append("Difference_{}".format(sampleStr))
+    insertColumnList.append("Difference_{}".format(sampleStr))
+
+    columnStr = "Direction_{} TEXT".format(sampleStr)
+    createColumnList.append(columnStr)
+    columnList.append("Direction_{}".format(sampleStr))
+    insertColumnList.append("Direction_{}".format(sampleStr))
+
+    columnStr = "Ratio_{} REAL".format(sampleStr)
+    createColumnList.append(columnStr)
+    columnList.append("Ratio_{}".format(sampleStr))
+    insertColumnList.append("Ratio_{}".format(sampleStr))
+
+    columnStr = "Group_{} TEXT".format(sampleStr)
+    createColumnList.append(columnStr)
+    columnList.append("Group_{}".format(sampleStr))
+    insertColumnList.append("Group_{}".format(sampleStr))
+
+pathlib.Path(sqlFolderStr.format(branch=branchStr)).mkdir(parents=True,exist_ok=True)
+sqlPath = sqlPathStr.format(branch=branchStr,ant=antStr)
+Connect = sqlite3.connect(sqlPath)
+Cursor = Connect.cursor()
+ReturnMsg = Cursor.execute(createComStr.format(",".join(createColumnList)))
+Connect.commit()
+print("[SQLite3:CreateTable] "+sqlPath)
+
+insertComStr = "INSERT INTO FPKM_DifferencesSummary ({column}) VALUES ({value})"
+a = 0
+for uuid in diffrDict.keys():
+    valueList = list()
+    valueList.append(uuid)
+    for posInt in range(len(insertColumnList)):
+        valueList.append(diffrDict[uuid][insertColumnList[posInt]])
+
+    insertCommand = insertComStr.format(column=",".join(columnList),value=(("?,"*(len(valueList)-1)))+"?")
+    ReturnMsg = Cursor.execute(insertCommand,valueList)
+
+Connect.commit()
+print("[SQLite3:Insert] "+sqlPath)
+Connect.close()
+print("[SQLite3:Close]\n")
+
+print("\n-- Exporting of FPKM differences per sample --")
+for sampleStr in sampleList:
+    sampleCreateComStr = "CREATE TABLE FPKM_Differences_{} ({})"
+    sampleCreateColumnList = [
+        "'UUID'  TEXT PRIMARY KEY NOT NULL", 
+        "'TranscriptName'  TEXT", 
+        "'GeneID'  TEXT", 
+    ]
+    sampleInsertList = ["UUID","TranscriptName","GeneID"]
+
+    columnStr = "FPKM_{}_Q20 REAL".format(sampleStr)
+    sampleCreateColumnList.append(columnStr)
+    sampleInsertList.append("FPKM_{}_Q20".format(sampleStr))
+
+    columnStr = "FPKM_{}_Q30 REAL".format(sampleStr)
+    sampleCreateColumnList.append(columnStr)
+    sampleInsertList.append("FPKM_{}_Q30".format(sampleStr))
+
+    columnStr = "Difference_{} REAL".format(sampleStr)
+    sampleCreateColumnList.append(columnStr)
+    sampleInsertList.append("Difference_{}".format(sampleStr))
+
+    columnStr = "Direction_{} TEXT".format(sampleStr)
+    sampleCreateColumnList.append(columnStr)
+    sampleInsertList.append("Direction_{}".format(sampleStr))
+
+    columnStr = "Ratio_{} REAL".format(sampleStr)
+    sampleCreateColumnList.append(columnStr)
+    sampleInsertList.append("Ratio_{}".format(sampleStr))
+
+    columnStr = "Group_{} TEXT".format(sampleStr)
+    sampleCreateColumnList.append(columnStr)
+    sampleInsertList.append("Group_{}".format(sampleStr))
+
+    sampleDict = dict()
+    for uuid in diffrDict.keys():
+        subDict = dict()
+        subDict.update({ 'UUID' : uuid })
+        subDict.update({ 'TranscriptName' : tidDict[uuid]["TranscriptName"] })
+        subDict.update({ 'GeneID' : tidDict[uuid]["GeneID"] })
+        subDict.update({ 'FPKM_{}_Q20'.format(sampleStr) : q20Dict[uuid][sampleStr] })
+        subDict.update({ 'FPKM_{}_Q30'.format(sampleStr) : q30Dict[uuid][sampleStr] })
+        subDict.update({ 'Difference_{}'.format(sampleStr) : diffrDict[uuid]["Difference_{}".format(sampleStr)] })
+        subDict.update({ 'Direction_{}'.format(sampleStr) : diffrDict[uuid]["Direction_{}".format(sampleStr)] })
+        subDict.update({ 'Ratio_{}'.format(sampleStr) : diffrDict[uuid]["Ratio_{}".format(sampleStr)] })
+        subDict.update({ 'Group_{}'.format(sampleStr) : diffrDict[uuid]["Group_{}".format(sampleStr)] })
+        sampleDict.update({ uuid : subDict })
+
+    sqlPath = sqlPathStr.format(branch=branchStr,ant=antStr)
+    Connect = sqlite3.connect(sqlPath)
+    Cursor = Connect.cursor()
+    ReturnMsg = Cursor.execute(sampleCreateComStr.format(sampleStr,",".join(sampleCreateColumnList)))
+    Connect.commit()
+    print("[SQLite3:CreateTable] "+sqlPath)
+
+    for uuid in diffrDict.keys():
+        valueList = list()
+        for posInt in range(len(sampleInsertList)):
+            valueList.append(sampleDict[uuid][sampleInsertList[posInt]])
+
+        sampleInsertComStr = "INSERT INTO FPKM_Differences_{sample} ({column}) VALUES ({value})"
+        insertCommand = sampleInsertComStr.format(sample=sampleStr, column=",".join(sampleInsertList),value=(("?,"*(len(valueList)-1)))+"?")
+        ReturnMsg = Cursor.execute(insertCommand,valueList)
+
+    Connect.commit()
+    print("[SQLite3:Insert] "+sqlPath)
+    Connect.close()
+    print("[SQLite3:Close]\n")
--- 07-sl1-Convert2SQLite3-sampleRun.py
+++ 07-sl1-Convert2SQLite3-sampleRun.py
@@ -8,23 +8,27 @@ branchStr = "testing"
 conditionList = [("speciesTestingA","trimQ20"),("speciesTestingA","trimQ30")]
 controlStr = "Controlr1"
 sampleList = ["Controlr1","T1r1","T2r1","T3r1","T4r1","T5r1"]
+
 compareSet = set()
-stringtiePathStr = "data/05-st-stringtie2/{branch}/{ant}-{trim}/{sample}-expression.tsv"
-sqlFolderStr = "data/07-sl-expressionTable-SQLite3/{branch}/"
-sqlPathStr = 'data/07-sl-expressionTable-SQLite3/{branch}/Expression-{ant}-{trim}.db'
+geneExpPathStr = "data/05-ds-stringtie2/{branch}/{ant}-{trim}/{sample}-expression.tsv"
+transcriptExpPathStr = "data/05-ds-ballgown/{branch}/{ant}-{trim}-{sample}/t_data.ctab"
+sqlFolderStr = "data/07-sl-expressionTable-SQLite3/{branch}-ds/"
+pathlib.Path( sqlFolderStr.format(branch=branchStr) ).mkdir(parents=True,exist_ok=True)
+sqlPathStr = 'data/07-sl-expressionTable-SQLite3/{branch}-ds/Expression-{ant}-{trim}.db'
 
 for conditionTup in conditionList:
     antStr = conditionTup[0]
     trimStr = conditionTup[1]
     for sampleStr in sampleList:
-        samplePath = stringtiePathStr.format(
+        print("-- Data format conversion for Gene Expression --")
+        geneSamplePath = geneExpPathStr.format(
                 branch=branchStr,
                 ant=antStr,
                 trim=trimStr,
                 sample=sampleStr
             )
-        sampleDF = pd.read_csv(samplePath,delimiter="\t",header=0)
-        print("[Pandas]\n    "+samplePath)
+        sampleDF = pd.read_csv(geneSamplePath,delimiter="\t",header=0)
+        print("[Pandas:Read]"+geneSamplePath)
         
         rowList = sampleDF.values.tolist()
         countInt = len(rowList)
@@ -47,12 +51,10 @@ for conditionTup in conditionList:
         elif compareSet == set(compareList):
             print("    "+sampleStr+": Different")
 
-        pathlib.Path( sqlFolderStr.format(branch=branchStr) ).mkdir(parents=True,exist_ok=True)
         sqlPath = sqlPathStr.format(branch=branchStr,ant=antStr,trim=trimStr)
         Connect = sqlite3.connect(sqlPath)
-        print("[SQLite3]\n    "+sqlPath)
         Cursor = Connect.cursor()
-        ReturnMsg = Cursor.execute("""CREATE TABLE {}_Expression
+        ReturnMsg = Cursor.execute("""CREATE TABLE GeneExpression_{}
                     ('UUID'  TEXT    PRIMARY KEY NOT NULL, 
                     'GeneID'   TEXT    NOT NULL,
                     'GeneName' TEXT    NOT NULL, 
@@ -64,8 +66,9 @@ for conditionTup in conditionList:
                     'FPKM'  REAL    NOT NULL, 
                     'TPM'   REAL    NOT NULL);""".format(sampleStr))
         Connect.commit()
+        print("[SQLite3:CreateTable] "+sqlPath)
 
-        insertComStr = "INSERT INTO {}_Expression ('UUID','GeneID','GeneName','Reference','Strand','Start','End','Coverage','FPKM','TPM')\
+        insertComStr = "INSERT INTO GeneExpression_{} ('UUID','GeneID','GeneName','Reference','Strand','Start','End','Coverage','FPKM','TPM')\
                         VALUES (?,?,?,?,?,?,?,?,?,?)".format(sampleStr)
         for rowInt in range(countInt):
             insertList = []
@@ -75,9 +78,79 @@ for conditionTup in conditionList:
             ReturnMsg = Cursor.execute(insertComStr,insertList)
 
         Connect.commit()
+        print("[SQLite3:Insert] "+sqlPath)
         Connect.close()
+        print("[SQLite3:Close]\n")
 
-    createComStr = "CREATE TABLE ExpressionSummary ({})"
+        # Transcript
+        print("-- Data format conversion for Transcript Expression --")
+
+        transcriptSamplePath = transcriptExpPathStr.format(
+                branch=branchStr,
+                ant=antStr,
+                trim=trimStr,
+                sample=sampleStr
+            )
+        sampleDF = pd.read_csv(transcriptSamplePath,delimiter="\t",header=0)
+        print("[Pandas:Read] "+transcriptSamplePath)
+        
+        rowList = sampleDF.values.tolist()
+        countInt = len(rowList)
+        
+        # check
+        compareList = list()
+        for rowInt in range(countInt):
+            insertList = []
+            sourceList = rowList[rowInt]
+            insertList.append("UUID."+str(rowInt))
+            insertList.extend(sourceList[0:10])
+            compareStr = "\t".join([str(x) for x in insertList])
+            compareList.append(compareStr)
+        
+        if compareSet == set():
+            print("    "+sampleStr+": Empty")
+            compareSet = set(compareList)
+        elif compareSet != set(compareList):
+            print("    "+sampleStr+": Same")
+        elif compareSet == set(compareList):
+            print("    "+sampleStr+": Different")
+
+        sqlPath = sqlPathStr.format(branch=branchStr,ant=antStr,trim=trimStr)
+        Connect = sqlite3.connect(sqlPath)
+        Cursor = Connect.cursor()
+        ReturnMsg = Cursor.execute("""CREATE TABLE TranscriptExpression_{}
+                    ('UUID'     TEXT    PRIMARY KEY NOT NULL, 
+                    'TranscriptID'  INTEGER    NOT NULL,
+                    'Chromosome'    TEXT, 
+                    'Strand'    TEXT    NOT NULL, 
+                    'Start' INTEGER NOT NULL, 
+                    'End'   INTEGER NOT NULL, 
+                    'TranscriptName'    TEXT    NOT NULL, 
+                    'ExonCount'    INTEGER  NOT NULL, 
+                    'Length'    INTEGER  NOT NULL, 
+                    'GeneID'    TEXT  NOT NULL, 
+                    'GeneName'    TEXT  NOT NULL, 
+                    'Coverage'  REAL    NOT NULL, 
+                    'FPKM'  REAL    NOT NULL);""".format(sampleStr))
+        Connect.commit()
+        print("[SQLite3:CreateTable] "+sqlPath)
+
+        insertComStr = "INSERT INTO TranscriptExpression_{} ('UUID','TranscriptID','Chromosome','Strand','Start','End','TranscriptName','ExonCount','Length','GeneID','GeneName','Coverage','FPKM')\
+                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)".format(sampleStr)
+        for rowInt in range(countInt):
+            insertList = []
+            sourceList = rowList[rowInt]
+            insertList.append("UUID."+str(rowInt))       
+            insertList.extend(sourceList)
+            ReturnMsg = Cursor.execute(insertComStr,insertList)
+
+        Connect.commit()
+        print("[SQLite3:Insert] "+sqlPath)
+        Connect.close()
+        print("[SQLite3:Close]\n")
+
+    print("-- Summarising for Gene Expression --")
+    createComStr = "CREATE TABLE GeneExpressionSummary ({})"
     createColumnList = [
         "'UUID'  TEXT PRIMARY KEY NOT NULL", 
         "'GeneID' TEXT",
@@ -91,13 +164,13 @@ for conditionTup in conditionList:
             insertColumnList.append("{target}_{sample}".format(target=targetStr,sample=sampleStr))
 
     Connect = sqlite3.connect(sqlPath)
-    print("[SQLite3]\n    "+sqlPath)
     Cursor = Connect.cursor()
     ReturnMsg = Cursor.execute(createComStr.format(",".join(createColumnList)))
     Connect.commit()
+    print("[SQLite3:CreateTable] "+sqlPath)
 
     resultDict = dict()
-    controlExc = Cursor.execute("SELECT UUID, GeneID, GeneName from {}_Expression".format(controlStr))
+    controlExc = Cursor.execute("SELECT UUID, GeneID, GeneName from GeneExpression_{}".format(controlStr))
     for rowList in controlExc:
         uuid, geneid, genename = rowList
         subDict = {
@@ -109,7 +182,7 @@ for conditionTup in conditionList:
 
 
     for sampleStr in sampleList:
-        sampleExc = Cursor.execute("SELECT UUID, FPKM, TPM  from {}_Expression".format(sampleStr))
+        sampleExc = Cursor.execute("SELECT UUID, FPKM, TPM  from GeneExpression_{}".format(sampleStr))
         for rowList in sampleExc:
             uuid, fpkm, tpm = rowList
             subDict = resultDict[uuid]
@@ -120,7 +193,7 @@ for conditionTup in conditionList:
             resultDict.update({ uuid : subDict })
 
 
-    insertComStr = "INSERT INTO ExpressionSummary ({column}) VALUES ({value})"
+    insertComStr = "INSERT INTO GeneExpressionSummary ({column}) VALUES ({value})"
     a = 0
     for uuid in resultDict.keys():
         valueList = list()
@@ -131,4 +204,68 @@ for conditionTup in conditionList:
         ReturnMsg = Cursor.execute(insertCommand,valueList)
 
     Connect.commit()
+    print("[SQLite3:Insert] "+sqlPath)
     Connect.close()
+    print("[SQLite3:Close]\n")
+
+    # Transcript
+    print("-- Summarising for Transcript Expression --")
+    createComStr = "CREATE TABLE TranscriptExpressionSummary ({})"
+    createColumnList = [
+        "'UUID'  TEXT PRIMARY KEY NOT NULL", 
+        "'TranscriptID' INTEGER",
+        "'TranscriptName' TEXT",
+        "'GeneID' TEXT",
+        "'GeneName' TEXT",
+    ]
+    insertColumnList = ["UUID", "TranscriptID", "TranscriptName", "GeneID", "GeneName"]
+    for sampleStr in sampleList:
+        columnStr = "FPKM_{sample} REAL".format(sample=sampleStr)
+        createColumnList.append(columnStr)
+        insertColumnList.append("FPKM_{sample}".format(sample=sampleStr))
+
+    Connect = sqlite3.connect(sqlPath)
+    Cursor = Connect.cursor()
+    ReturnMsg = Cursor.execute(createComStr.format(",".join(createColumnList)))
+    Connect.commit()
+    print("[SQLite3:CreateTable] "+sqlPath)
+
+    resultDict = dict()
+    controlExc = Cursor.execute("SELECT UUID, TranscriptID, TranscriptName, GeneID, GeneName from TranscriptExpression_{}".format(controlStr))
+    for rowList in controlExc:
+        uuid, tid, tname, geneid, genename = rowList
+        subDict = {
+            "UUID" : uuid,
+            "TranscriptID" : tid,
+            "TranscriptName" : tname,
+            "GeneID" : geneid,
+            "GeneName" : genename
+        }
+        resultDict.update({ uuid : subDict })
+
+
+    for sampleStr in sampleList:
+        sampleExc = Cursor.execute("SELECT UUID, FPKM  from TranscriptExpression_{}".format(sampleStr))
+        for rowList in sampleExc:
+            uuid, fpkm = rowList
+            subDict = resultDict[uuid]
+            subDict.update({
+                "FPKM_{}".format(sampleStr) : fpkm
+            })
+            resultDict.update({ uuid : subDict })
+
+
+    insertComStr = "INSERT INTO TranscriptExpressionSummary ({column}) VALUES ({value})"
+    a = 0
+    for uuid in resultDict.keys():
+        valueList = list()
+        for posInt in range(len(insertColumnList)):
+            valueList.append(resultDict[uuid][insertColumnList[posInt]])
+
+        insertCommand = insertComStr.format(column=",".join(insertColumnList),value=(("?,"*(len(valueList)-1)))+"?")
+        ReturnMsg = Cursor.execute(insertCommand,valueList)
+
+    Connect.commit()
+    print("[SQLite3:Insert] "+sqlPath)
+    Connect.close()
+    print("[SQLite3:Close]\n")
